e793b2c0f1b632ac7093719956b6ca3b6b949d73
Paul Zavattieri <paulz92@gmail.com>
2017-10-10 17:33:32 -0400

    wrote html, css, pseudocoded javascript, added images

---
+++ assets/JavaScript/game.js
@@ -0,0 +1,9 @@
+// Pseudocode
+// Hangman begins, a word is picked, and underlines appear on the screen for the amount of letters in that word. no characters appear
+// user guesses the letters in the word by typing on the key board
+// user gets 12 guesses before game is over
+// if user guesses a letter that's in the word, that letter appears where it's respective underline was (or underlineS). guesses decreases by 1
+// if user guesses incorrectly, guesses decreases by one, and letter appears under 'guessed letters' section
+// if user repeats an already guessed letter, nothing happens. no deduction from guesses occurs
+// if user guesses all letters correctly before guesses run out, display that they win
+// if user depletes guesses before word is picked, game over. display that they lose, and display correct answer
